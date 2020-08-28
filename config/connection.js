4cb9fe15680fec8a6925e984b2f16296a83b3815
Stefan Salituro <stefansalituro@gmail.com>
2020-08-27 20:46:47 -0400

    Second commit - established the connection to the database created with node and MySQL using the javascript file connection.js.

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,30 @@
-var mysql = require('mysql');
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+
+
+// Require mysql
+var mysql = require("mysql");
+
+// Set up our connection information
 var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+};
+
+
+// Connect to 'burgers_db' database created in MySQL
+connection.connect(function(err) {
+  if(err)throw err;
+  console.log("connected as id: " + connection.threadId);
+});
+
+// Export connection
+module.exports = connection;
 
