470eae9804ed2ca5a391d257ae0642a1b3ab5833
Stefan Salituro <stefansalituro@gmail.com>
2020-08-27 20:53:46 -0400

    Third commit - orm.js file created - now have to link connection.js to the new orm.js file.

--- server.js
+++ server.js
@@ -10,3 +10,16 @@ app.use(express.static(process.cwd() + '/public'));
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
+
+// using handlebars
+// override the method with POST having - -  **?_method=DELETE**
+app.use(methodOverride('_method'));
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Use the require method once everything is set
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
