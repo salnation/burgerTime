5a7d28f9cce633fcd1bd11f24f3d751bc168ffdf
Stefan Salituro <stefansalituro@gmail.com>
2020-08-27 21:34:28 -0400

    orm.js completed - added in update method to make the app devour the burgers_db.

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,20 @@
+// Here is the orm.js where you write functions that takes inputs and conditions and turn them into database commands like SQL.
+
 // orm.all - this collects all the data from our newly created DATABASE
 
 // will be creating a connection query that will select everything from the table input
 
 var connection = require('./connection.js');
+
+function printQuestionMarks(num) {
+	var arr = [];
+
+	for (var i = 0; i < num; i++) {
+		arr.push('?');
+	}
+
+	return arr.toString();
+}
 
 //uses a callback to pass the result into the next file in the models folder
 // tableInput
@@ -13,3 +25,22 @@ var orm = {
       cb(result)
     })
   },
+
+  // 'update:' method addition 
+
+  update: function(tableInput, condition, cb){
+    connection.query('UPDATE '+tableInput+' SET devoured=true WHERE id='+condition+';', function(err,result){
+        if(err)throw err;
+        cb(result);
+      })
+    },
+  
+    create: function(tableInput,val,cb){
+      connection.query('INSERT INTO '+tableInput+"(burger_name) VALUES ('"+val+"');", function(err,result){
+        if(err)throw err;
+        cb(result);
+      })
+    }
+  }
+  
+  module.exports = orm;
